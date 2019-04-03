2234850c01b10d92e183ad1635fba52c38bc8be1
peutes <peutes@gmail.com>
2019-04-03 18:36:05 +0900

    add black and models and static

--- app/models.py
+++ app/models.py
@@ -2,7 +2,23 @@ from django.db import models
 
 
 class Book(models.Model):
-  """書籍"""
-  name = models.CharField(verbose_name='書籍名', max_length=255)
-  publisher = models.CharField(verbose_name='出版社', max_length=255, blank=true)
-  page = models.IntegerField(verbose_name='ページ数', )
+    """書籍"""
+
+    name = models.CharField("書籍名", max_length=255)
+    publisher = models.CharField("出版社", max_length=255, blank=True)
+    page = models.IntegerField("ページ数", blank=True, default=0)
+
+    def __str__(self):
+        return self.name + ": 文字列です"
+
+
+class Impression(models.Model):
+    """感想"""
+
+    book = models.ForeignKey(
+        Book, verbose_name="書籍", related_name="impressions", on_delete=models.CASCADE
+    )
+    comment = models.TextField("コメント", blank=True)
+
+    def __str__(self):
+        return self.comment + ": 文字列です"
