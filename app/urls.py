f05c7a2f9c971d266dfe257f69eb4d6920055b2f
peutes <peutes@gmail.com>
2019-04-03 20:42:56 +0900

    post form

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ app_name = "app"  # htmlからURLを生成する時に使う名前空間
 urlpatterns = [
     # 書籍
     path("book/", views.book_list, name="book_list"),  # 一覧
-    path("book/add/", views.book_add, name="book_add"),  # 登録
+    path("book/add/", views.book_edit, name="book_add"),  # 登録
     path("book/mod/<int:book_id>/", views.book_edit, name="book_edit"),  # 修正
     path("book/del/<int:book_id>/", views.book_del, name="book_del"),  # 削除
 ]
--- app/views.py
+++ app/views.py
@@ -1,20 +1,37 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render_to_response, redirect
 
+from app.forms import BookForm
 from app.models import Book
 
 
 def book_list(request):
     books = Book.objects.all().order_by("id")
-    return render(request, "app/book_list.html", {"books": books})
-
-
-def book_add(request, book_id=None):
-    return HttpResponse("書籍の編集")
+    return render_to_response("app/book_list.html", dict(books=books))
 
 
 def book_edit(request, book_id=None):
-    return HttpResponse("書籍の編集")
+    # 既存Bookの取得
+    if book_id:  # book_id が指定されている (修正時)
+        book = get_object_or_404(Book, pk=book_id)
+    else:  # book_id が指定されていない (追加時)
+        book = Book()
+
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+
+        if form.is_valid():
+            book = form.save(commit=False)
+
+            # なにかやる
+
+            book.save()
+            return redirect("app:book_list")
+
+    else:  # GET
+        form = BookForm(instance=book)
+
+    return render_to_response("app/book_edit.html", dict(form=form, book_id=book_id))
 
 
 def book_del(request, book_id):
