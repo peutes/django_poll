6b8cb4f716b8a0870365327998c9a2a397ea1c27
peutes <peutes@gmail.com>
2019-04-03 19:46:38 +0900

    use templates

--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from app.models import Book
+
+
+def book_list(request):
+    books = Book.objects.all().order_by("id")
+    return render(request, "app/book_list.html", {"books": books})
+
+
+def book_add(request, book_id=None):
+    return HttpResponse("書籍の編集")
+
+
+def book_edit(request, book_id=None):
+    return HttpResponse("書籍の編集")
+
+
+def book_del(request, book_id):
+    return HttpResponse("書籍の削除")
