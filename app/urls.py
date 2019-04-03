91f7d9cf3a2b733d8726ef9f35523b86f491093b
peutes <peutes@gmail.com>
2019-04-03 21:12:39 +0900

    delete

--- app/urls.py
+++ app/urls.py
@@ -6,8 +6,8 @@ app_name = "app"  # htmlからURLを生成する時に使う名前空間
 
 urlpatterns = [
     # 書籍
-    path("book/", views.book_list, name="book_list"),  # 一覧
-    path("book/add/", views.book_edit, name="book_add"),  # 登録
-    path("book/mod/<int:book_id>/", views.book_edit, name="book_edit"),  # 修正
-    path("book/del/<int:book_id>/", views.book_del, name="book_del"),  # 削除
+    path("books/", views.book_list, name="book_list"),  # 一覧
+    path("books/add/", views.book_edit, name="book_add"),  # 登録
+    path("books/mod/<int:book_id>/", views.book_edit, name="book_edit"),  # 修正
+    path("books/del/<int:book_id>/", views.book_del, name="book_del"),  # 削除
 ]
--- app/views.py
+++ app/views.py
@@ -1,38 +1,40 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render_to_response, redirect
+from django.db.models import QuerySet
+from django.shortcuts import get_object_or_404, render_to_response, redirect, render
 
 from app.forms import BookForm
 from app.models import Book
 
 
 def book_list(request):
-    books = Book.objects.all().order_by("id")
-    return render_to_response("app/book_list.html", dict(books=books))
+    books: QuerySet[Book] = Book.objects.all().order_by("id")
+    return render_to_response("app/book_list.html", {"books": books})
 
 
 def book_edit(request, book_id=None):
-    # 既存Bookの取得
     if book_id:  # book_id が指定されている (修正時)
-        book = get_object_or_404(Book, pk=book_id)
+        book: Book = get_object_or_404(Book, pk=book_id)
     else:  # book_id が指定されていない (追加時)
-        book = Book()
+        book: Book = Book()
 
+    # 既存Bookの取得
     if request.method == "POST":
-        form = BookForm(request.POST, instance=book)
-
+        form: BookForm = BookForm(request.POST, instance=book)
         if form.is_valid():
-            book = form.save(commit=False)
+            # book = form.save(commit=False)  # commit=False にするとDBにSaveされない
+            # # なにかbookを編集する
+            # book.save()
 
-            # なにかやる
-
-            book.save()
+            # ModelFormからModel.save_m2m()を内部で呼び出して保存される
+            form.save()
             return redirect("app:book_list")
 
     else:  # GET
         form = BookForm(instance=book)
 
-    return render_to_response("app/book_edit.html", dict(form=form, book_id=book_id))
+    return render(request, "app/book_edit.html", {"form": form, "book_id": book_id})
 
 
 def book_del(request, book_id):
-    return HttpResponse("書籍の削除")
+    book: Book = get_object_or_404(Book, pk=book_id)
+    book.delete()
+    return redirect("app:book_list")
